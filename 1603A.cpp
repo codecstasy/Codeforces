1e9705d64936a62d1987e1ba25c15d6a441f460f
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2021-11-11 12:10:21 +0600

    Add files via upload
    Solution to the problem 1603A - Di-Visible Confusion in codeforces in C++

---
+++ 1603A.cpp
@@ -0,0 +1,52 @@
+/*
+             CodEcstasy
+            SUST SWE-19
+*/
+#include <bits/stdc++.h>
+#define ll long long
+#define s second
+#define f first
+#define fora(i,n) for(int i=0 ; i<n ; i++)
+#define ford(i,n) for(int i=n-1 ; i>=0 ; i--)
+#define b begin
+#define e end
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+using namespace std;
+
+void solve() {
+	ll n;
+	std::cin >> n;
+   	vector <ll> ar(n);
+    for(ll i=0 ; i<n ; i++) {
+    	cin >> ar[i];
+    }
+    //for(ll i=0 ; i<n ; i++) cout << ar[i] << " " << ind[i] << endl;
+    bool out=true;
+    for(ll i=0 ; i<n ; i++) {
+    	ll j=i+2;
+    	bool in=false;
+    	while(j>=2) {
+    		if(ar[i] % (j--) != 0) {
+    			in=true;
+				break;
+    		}
+		}
+    	out &= in;
+    }
+    if(out)
+    	cout << "YES" << endl;
+    else
+    	cout << "NO" << endl;
+}
+
+int main() 
+{
+    ll t, n;
+    t = 1;
+    std::cin >> t;
+    while( t-- ) {
+        solve();
+    }
+    return 0;
+}
